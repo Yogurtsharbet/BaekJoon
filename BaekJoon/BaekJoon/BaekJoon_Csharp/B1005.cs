6a2599423c5d4e85277dd9b06ad97cb020cfa10c
전홍현 <toinbee3@naver.com>
2025-01-29 22:26:54 +0900

    B 1005 OnGoing

---
+++ BaekJoon/BaekJoon/BaekJoon_Csharp/B1005.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+/// <summary>
+/// <para>Q. 각 시간이 정의된 노드에서 특정 노드에 도달하기 까지 걸리는 최소 시간</para>
+/// <c>INPUT</c><br/>
+/// <see cref="T"/> : Number of Test Case <br/>
+/// <see cref="N"/> : Number of Buildings <br/>
+/// <see cref="K"/> : Number of Paths <br/>
+/// </summary>
+public class B1005 {
+    private int T;
+    private int N;
+    private int K;
+}
+
